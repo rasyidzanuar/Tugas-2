e3add6bb6387fe3bae95ede0f58c774c83c51681
rasyidzanuar <126134959+rasyidzanuar@users.noreply.github.com>
2023-03-24 05:54:24 +0700

    Create finobaci if.php

---
+++ finobaci if.php
@@ -0,0 +1,30 @@
+<?php
+
+function fibonacci (int $jumlahBilangan) {
+  # array ini akan digunakan untuk menampung bilangan fibonacci
+  $fibonacci = [];
+
+  if ($jumlahBilangan < 0) {
+    # langsung hentikan fungsi jika $jumlahBilangan kurang dari 0
+    return $fibonacci; 
+  }
+
+  # mulai perulangan
+  for ($i = 0; $i < $jumlahBilangan; $i++) {
+    if ($i < 2) {
+      $bilangan = $i;
+    } else {
+      $bilangan = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+    }
+
+    # tambahkan $bilangan ke dalam array $fibonacci
+    array_push($fibonacci, $bilangan);
+  }
+
+  return $fibonacci;
+}
+
+# panggil fungsi fibonacci dengan berbagai parameter
+echo implode(" ", fibonacci(20));
+
+?>
